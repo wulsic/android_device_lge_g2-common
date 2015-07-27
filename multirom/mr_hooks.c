0a1e2eb3304a12c1726d2199cf26ceebeaf74541
Erwan Leboucher <erwanleboucher@gmail.com>
2015-07-27 18:07:22 +0200

    test

--- multirom/mr_hooks.c
+++ multirom/mr_hooks.c
@@ -6,187 +6,102 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
 #include <sys/stat.h>
-#include <stdint.h>
-#include <time.h>
-
-#include <log.h>
 
 #if MR_DEVICE_HOOKS >= 1
 int mrom_hook_after_android_mounts(const char *busybox_path, const char *base_path, int type)
 {
-    return 0;
+    // On M7, this fstab file is used to remount system to RO,
+    // but with MultiROM, it remounts everything as RO, even /data and /cache
+    if(access("/remount.qcom", F_OK) >= 0)
+        remove("/remount.qcom");
+
+    // remove mounting from .rc files
+    int res = -1;
+    int add_dummy = 0;
+    char *l;
+    char line[1024];
+    char path_in[128];
+    char path_out[128];
+    FILE *in, *out;
+    DIR *d;
+    struct dirent *dp;
+
+    d = opendir("/");
+    if(!d)
+        return -1;
+
+    while((dp = readdir(d)))
+    {
+        if(dp->d_type != DT_REG || !strstr(dp->d_name, ".rc"))
+            continue;
+
+        snprintf(path_out, sizeof(path_out), "/%s", dp->d_name);
+        snprintf(path_in, sizeof(path_in), "/%s.new", dp->d_name);
+
+        if(rename(path_out, path_in) < 0)
+            goto exit;
+
+        in = fopen(path_in, "r");
+        if(!in)
+            goto exit;
+
+        out = fopen(path_out, "w");
+        if(!out)
+        {
+            fclose(in);
+            goto exit;
+        }
+
+        while((fgets(line, sizeof(line), in)))
+        {
+            for(l = line; isspace(*l); ++l);
+
+            if (strncmp(l, "on ", 3) == 0)
+                add_dummy = 1;
+            else if (strstr(l, "mount ") &&
+                (strstr(l, "/data") || strstr(l, "/system") || strstr(l, "/cache")))
+            {
+                if(add_dummy == 1)
+                {
+                    add_dummy = 0;
+                    fputs("    export DUMMY_LINE_INGORE_IT 1\n", out);
+                }
+
+                fputc((int)'#', out);
+            }
+
+            fputs(line, out);
+        }
+
+        fclose(out);
+        fclose(in);
+        remove(path_in);
+
+        chmod(path_out, 0750);
+    }
+
+    res = 0;
+exit:
+    closedir(d);
+    return res;
 }
 #endif /* MR_DEVICE_HOOKS >= 1 */
 
 
 #if MR_DEVICE_HOOKS >= 2
-// Screen gets cleared immediatelly after closing the framebuffer on this device,
-// give user a while to read the message box until it dissapears.
 void mrom_hook_before_fb_close(void)
 {
-    usleep(800000);
 }
 #endif /* MR_DEVICE_HOOKS >= 2 */
 
 
 #if MR_DEVICE_HOOKS >= 3
-static time_t gettime(void)
-{
-    struct timespec ts;
-    int ret;
-
-    ret = clock_gettime(CLOCK_MONOTONIC, &ts);
-    if (ret < 0) {
-        ERROR("clock_gettime(CLOCK_MONOTONIC) failed: %s\n", strerror(errno));
-        return 0;
-    }
-
-    return ts.tv_sec;
-}
-
-static int wait_for_file(const char *filename, int timeout)
-{
-    struct stat info;
-    time_t timeout_time = gettime() + timeout;
-    int ret = -1;
-    int waited = 0;
-
-    while (gettime() < timeout_time && ((ret = stat(filename, &info)) < 0))
-    {
-        waited = 1;
-        usleep(30000);
-    }
-
-    if(ret >= 0)
-        return waited;
-    else
-        return ret;
-}
-
-static void wait_for_mmc(void)
-{
-    // boot
-    const char *filename = "/sys/devices/msm_sdcc.1/mmc_host/mmc1/mmc1:0001/block/mmcblk0/mmcblk0p19/uevent";
-    int ret;
-
-    INFO("Waiting for file %s\n", filename);
-    ret = wait_for_file(filename, 5);
-    if(ret > 0)
-    {
-        // had to wait, make sure the init is complete
-        usleep(100000);
-    }
-    else if(ret < 0)
-    {
-        ERROR("Timeout while waiting for %s!\n", filename);
-    }
-}
-
-static int read_file(const char *path, char *dest, int dest_size)
-{
-    int res = 0;
-    FILE *f = fopen(path, "r");
-    if(!f)
-        return res;
-
-    res = fgets(dest, dest_size, f) != NULL;
-    fclose(f);
-    return res;
-}
-
-static int write_file(const char *path, const char *what)
-{
-    int res = 0;
-    FILE *f = fopen(path, "w");
-    if(!f)
-        return res;
-
-    res = fputs(what, f) >= 0;
-    fclose(f);
-    return res;
-}
-
-static void set_cpu_governor(void)
-{
-    size_t i;
-    char buff[256];
-    static const char *governors[] = { "interactive", "ondemand" };
-
-    if(!read_file("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", buff, sizeof(buff)))
-        return;
-
-    if(strncmp(buff, "performance", 11) != 0)
-        return;
-
-    if(!read_file("/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors", buff, sizeof(buff)))
-        return;
-
-    write_file("/sys/module/msm_thermal/core_control/enabled", "0");
-    write_file("/sys/devices/system/cpu/cpu1/online", "1");
-    write_file("/sys/devices/system/cpu/cpu2/online", "1");
-    write_file("/sys/devices/system/cpu/cpu3/online", "1");
-
-    for(i = 0; i < sizeof(governors)/sizeof(governors[0]); ++i)
-    {
-        if(strstr(buff, governors[i]))
-        {
-            write_file("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", governors[i]);
-            write_file("/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor", governors[i]);
-            write_file("/sys/devices/system/cpu/cpu2/cpufreq/scaling_governor", governors[i]);
-            write_file("/sys/devices/system/cpu/cpu3/cpufreq/scaling_governor", governors[i]);
-            break;
-        }
-    }
-
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/up_threshold","90");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/sampling_rate","50000");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/io_is_busy","1");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor","4");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/down_differential","10");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/up_threshold_multi_core","70");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/down_differential_multi_core","3");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/optimal_freq","960000");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/sync_freq","960000");
-    write_file("/sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load","80");
-    write_file("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq","300000");
-    write_file("/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq","300000");
-    write_file("/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq","300000");
-    write_file("/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq","300000");
-
-    write_file("/sys/module/msm_thermal/core_control/enabled", "1");
-
-    write_file("/sys/module/lpm_resources/enable_low_power/l2", "2");
-    write_file("/sys/module/lpm_resources/enable_low_power/pxo", "1");
-    write_file("/sys/module/lpm_resources/enable_low_power/vdd_dig", "1");
-    write_file("/sys/module/lpm_resources/enable_low_power/vdd_mem", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu2/power_collapse/suspend_enabled", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu3/power_collapse/suspend_enabled", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled", "1");
-    write_file("/sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu2/standalone_power_collapse/suspend_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu3/standalone_power_collapse/suspend_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu2/standalone_power_collapse/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu3/standalone_power_collapse/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu0/retention/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu1/retention/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu2/retention/idle_enabled","1");
-    write_file("/sys/module/pm_8x60/modes/cpu3/retention/idle_enabled","1");
-}
-
 void tramp_hook_before_device_init(void)
 {
-    // Some hammerhead kernels are too fast and mmcblk initialization
-    // occurs a bit too late, wait for it.
-    wait_for_mmc();
-
-    // hammerhead's kernel has "performance" as default
-    set_cpu_governor();
 }
 #endif /* MR_DEVICE_HOOKS >= 3 */
 
@@ -195,4 +110,4 @@ int mrom_hook_allow_incomplete_fstab(void)
 {
     return 1;
 }
-#endif /* MR_DEVICE_HOOKS >= 4 */
+#endif
